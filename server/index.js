4d9ba23c8a0898d6caf748240975e463b0a9124e
Than Nguyenvu <thelensguy@gmail.com>
2020-02-28 15:35:08 -0800

    prepping for ec2 instance

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
+require('newrelic');
+require('dontenv').config()
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
+app.use(express.json())
 
-app.use(express.static('public'));
+const PROXY_PORT = process.env.PROXY_PORT || 8080;
+const RESERVATIONS_HOSTNAME = process.env.RESERVATIONS_HOSTNAME || 'localhost';
+const RESERVATIONS_PORT = process.env.RESERVATION_PORT || 4444
 
-const port = 3043;
+// Reservation
+app.get('/api/reservations/:restaurantId/dateTime/:dateTime', (req, res) => {
+  axios.get(`http://${RESERVATIONS_HOSTNAME}:${RESERVATIONS_PORT}/api/reservations/${req.params.restaurantId}/dateTime/${req.params.dateTime}`)
+  .then(response => response.data)
+  .then(data => res.send({data}))
+  .catch(err => console.log('error at proxy serving', err))
+})
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.use('/:restaurantId', express.static('public'));
+
+// app.get('/reservations/:restaurantId', (req, res) => {
+//   axios.get(`http://localhost:4444/api/reservations/1/dateTime/2020-02-23%2001:45:00-08`)
+  // .then(response => response.data)
+  // .then(data => res.send({data}))
+  // .catch(err => console.log('error at proxy serving', err))
+// })
+// app.use('/reservations/?id=:restaurantId', express.static('public'));
+
+// const port = 3043;
+
+app.listen(PROXY_PORT, () => {
+  console.log(`App listening on port ${PROXY_PORT}`);
 });
